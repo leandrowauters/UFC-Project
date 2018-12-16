85db52a27d0f5a9bad9c816b43ae48bc91bbda8b
John Doe <leandro.wauters@gmail.com>
2018-12-16 18:40:10 -0500

    Before News View Controller

---
+++ UFC-Fighter-Practice/View/UFCFighterNewsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UFCFighterNewsViewController.swift
+//  UFC-Fighter-Practice
+//
+//  Created by Leandro Wauters on 12/16/18.
+//  Copyright © 2018 Leandro Wauters. All rights reserved.
+//
+
+import UIKit
+
+class UFCFighterNewsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+}
